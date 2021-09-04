37cd50646e3559154ecd10c075633a94229f2a7d
danielerognini <danielerognini@gmail.com>
2021-09-04 17:14:35 +0200

    Enviroment classes have been restructured and initially implemented
    -- Made by Rognini --

--- GameEngine/ECS/Entity.h
+++ GameEngine/ECS/Entity.h
@@ -16,35 +16,35 @@ class Entity : public Activatable {
 public:
     explicit Entity(const std::string& texturePath, bool solid = true, bool active = true);
     
-    Collider& getCollider(const std::string& name);
-    
-    bool addCollider(const std::string& name, std::vector<Border> borders, const bool& active);
-    
-    bool removeCollider(const std::string& name);
-    
-    Sprite& getSprite();
-    
-    Transform& getTransform();
-    
-    Text& getText();
-    void setText(const Text& text);
-    
-    void update();
-    void draw();
-    
-    void resolveCollision(const Entity& externalEntity, const Vector2D& collisionVector);
-    
-    bool isSolid() const;
-    
-    void setSolid(bool solid);
-    
-    bool isCollidersActive();
-    
-    std::unordered_map<std::string, Collider>::iterator begin();
-    std::unordered_map<std::string, Collider>::iterator end();
+    virtual Collider& getCollider(const std::string& name);
 
-    const ManagerStatus& getManagerStatus() const;
-    void setManagerStatus(const ManagerStatus& managerStatus);
+    virtual bool addCollider(const std::string& name, std::vector<Border> borders, const bool& active);
+
+    virtual bool removeCollider(const std::string& name);
+
+    virtual Sprite& getSprite();
+
+    virtual Transform& getTransform();
+
+    virtual Text& getText();
+    virtual void setText(const Text& text);
+
+    virtual void update();
+    virtual void draw();
+
+    virtual void resolveCollision(const Entity& externalEntity, const Vector2D& collisionVector);
+
+    virtual bool isSolid() const;
+
+    virtual void setSolid(bool solid);
+
+    virtual bool isCollidersActive();
+
+    virtual std::unordered_map<std::string, Collider>::iterator begin();
+    virtual std::unordered_map<std::string, Collider>::iterator end();
+
+    virtual const ManagerStatus& getManagerStatus() const;
+    virtual void setManagerStatus(const ManagerStatus& managerStatus);
 
 private:
     std::unordered_map<std::string, Collider> colliders;
