e4760bc89697104be9f2c0647b184ef230f138f6
Lorenzo Dominici <lorenzo.dominici@stud.unifi.it>
2021-09-05 19:00:47 +0200

    Creation of class Menu and first implementation. -- Made by Dominici, Rognini--

--- GameSrc/Room.cpp
+++ GameSrc/Room.cpp
@@ -5,10 +5,10 @@
 
 Room::Room(const std::string& name, const std::string& roomTemplate, unsigned int priorityOffset, bool discovered, bool active) : name(name) {
     //TODO: template room file parsing.
-    std::list<std::string> layersName = {name + "_enviroment", name + "_doors", name + "_enemies", name + "_NPCs", name + "_pets", name + "_players", name + "_items", name + "_bullets"};
-    for (int i = 0; i < layersName.size(); i++) {
-        Engine::getInstance().addManager(*std::next(layersName.begin(), i), i + priorityOffset);
-        layers.emplace(*std::next(layersName.begin(), i), &Engine::getInstance().getManager(*std::next(layersName.begin(), i)));
+    std::list<std::string> layersNames = {name + "_enviroment", name + "_doors", name + "_enemies", name + "_NPCs", name + "_pets", name + "_players", name + "_items", name + "_bullets"};
+    for (int i = 0; i < layersNames.size(); i++) {
+        Engine::getInstance().addManager(*std::next(layersNames.begin(), i), i + priorityOffset);
+        layers.emplace(*std::next(layersNames.begin(), i), &Engine::getInstance().getManager(*std::next(layersNames.begin(), i)));
     }
 }
 
